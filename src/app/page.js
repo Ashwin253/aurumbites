4a8b506d6c98ba422966acd44282c474c89ba85e
Ashwin253 <ashwingeorge25@gmail.com>
2026-03-23 11:55:07 +0530

    updated shop with brands

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function HomePage() {
       <main>
         <Hero />
         <HoverListView/>
-        <ProductCategories />
+        {/* <ProductCategories /> */}
         <Brands/>
         <QualityBadges />
       </main>
